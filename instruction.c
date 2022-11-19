f3548b64a40c3dbe7d2be50472911b5568cb99e6
Valerian <1072977032@qq.com>
2022-11-19 22:31:36 +0800

    更新了instruction 并进行一下简单的git测试

--- instruction.c
+++ instruction.c
@@ -14,11 +14,10 @@ Instruction* ins_new(int op_num){
 Instruction *ins_new_binary_operator(int Op, Value *S1, Value *S2){
     Instruction* inst = ins_new(2);
     // 将 inst 这个 user 加入到 s1 和 s2 这两个 value 的 use_list
-
     Use* puse = user_get_operand_use(&inst->user, 0);
     use_set_value(puse, S1);
     puse = user_get_operand_use(&inst->user, 1);
-    use_set_value(puse, S1);
+    use_set_value(puse, S2);
     return inst;
 }
 
