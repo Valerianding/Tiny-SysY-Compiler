fa6d10c97a7538a77493aeca663a772651d31aa2
Bri1987 <1216754835@qq.com>
2023-01-27 10:24:06 +0800

    fix front_end

--- ir/instruction.c
+++ ir/instruction.c
@@ -67,6 +67,11 @@ void clear_tmp(char* tmp)
 
 //获取instruction.user.value并赋个名字
 Value *ins_get_value(Instruction *ins){
+    return &ins->user.value;
+}
+
+//获取instruction.user.value并赋个名字
+Value *ins_get_value_with_name(Instruction *ins){
     Value *v_tmp=&ins->user.value;
     sprintf(t_num, "%d", t_index++);
     strcat(t,t_num);
