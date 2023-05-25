71a889d8398d4fb87f1611c8b6960948a8e7c5a9
Valerian <1072977032@qq.com>
2023-05-25 16:59:43 +0800

    2023/5/25 finish

--- tacmain.c
+++ tacmain.c
@@ -57,7 +57,6 @@ int main(int argc, char* argv[]){
     init_insnode_stack(&S_or);
     this=(Symtab*) malloc(sizeof(Symtab));
     symtab_init(this);
-
     yyparse();
     return_index=0;
     TRoot=TRoot->left;
@@ -106,7 +105,7 @@ int main(int argc, char* argv[]){
     printf_llvm_ir(instruction_list,argv[4],1);
 
     for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next) {
-
+        DVNT(currentFunction);
         renameVariabels(currentFunction);
     }
 
@@ -131,9 +130,7 @@ int main(int argc, char* argv[]){
     }
 
     // Liveness 计算之后请注释掉我跑llvm
-
-    //printf_llvm_ir(instruction_list,argv[1],1);
-
+    printf_llvm_ir(instruction_list,argv[4],1);
 
 
 
@@ -144,14 +141,12 @@ int main(int argc, char* argv[]){
 //    printf("=======func inline end=======\n");
 
     //lsy_begin
-//    fix_array(instruction_list);
-
-//    printf_llvm_ir(instruction_list,argv[4],0);
-
+    fix_array(instruction_list);
+    printf_llvm_ir(instruction_list,argv[4],0);
     //lsy_end
 
     //ljw_begin
-//   reg_control(instruction_list,temp);
+    reg_control(instruction_list,temp);
     //修改all_in_memory开启/关闭寄存器分配
     //ljw_end
 
@@ -160,10 +155,10 @@ int main(int argc, char* argv[]){
     //    ljf_begin
 //    如果需要打印到文件里面，打开arm_open_file和arm_close_file,
 //    argv[3]里面直接给的就是汇编文件，直接打开就行，修改一下
-
-//    arm_open_file(argv[3]);
-//    arm_translate_ins(instruction_list,argv[3]);
-//    arm_close_file(argv[3]);
+//
+    arm_open_file(argv[3]);
+    arm_translate_ins(instruction_list,argv[3]);
+    arm_close_file(argv[3]);
     //    ljf_end
     return 0;
 }
