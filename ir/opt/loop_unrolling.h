f5f2d6b41957d16d8f1a6cfe54d8c0d5f5209396
Bri1987 <1216754835@qq.com>
2023-07-03 11:16:38 +0800

    大致完成基本块内循环展开

--- ir/opt/loop_unrolling.h
+++ ir/opt/loop_unrolling.h
@@ -8,5 +8,19 @@
 #include "Loop.h"
 
 void loop_unroll(Function *currentFunction);
+//复制一个信息都相同的value副本
+Value *copy_value(Value *v_source,int index);
+//检查init、step、end是否都是int常数
+bool check_idc(Loop* loop);
+//计算迭代次数
+int cal_times(int init,int step,int end);
+//计算一次迭代的ir总数
+int cal_ir_cnt(HashSet *loopBody);
+
+bool check_in_latch_phi(Value *v,Value *variable,HashMap* latch_phi_map,HashMap* head_map);
+Value *get_replace_value(HashSet* set,int block_id);
+void update_replace_value(HashMap* map,Value* v_before,Value* v_replace);
+void LOOP_UNROLL_EACH(Loop* loop);
+void dfsLoop(Loop *loop);
 
 #endif //C22V1_LOOP_UNROLLING_H
