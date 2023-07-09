ad54aba5243e7aed803a00e17b7cd8d1b44a5586
Valerian <1072977032@qq.com>
2023-07-09 14:13:06 +0800

    2023/7/6 help arm.c

--- test/test1.c
+++ test/test1.c
@@ -47,17 +47,65 @@
 
 
 //test float
-int g = 0;
+//int g = 0;
+//
+//float func(int n) {
+//    g = g + n;
+//    putint(g);
+//    return g;
+//}
+//
+//int main() {
+//    int i;
+//    i = getint();
+//    if (!func(99) && func(100)) i = 1; else i = 0;
+//    return 0;
+//}
 
-float func(int n) {
-    g = g + n;
-    putint(g);
-    return g;
+const int maxN = 1005;
+int parent[maxN];
+
+int find(int root) {
+    if (parent[root] == root)
+        return root;
+    else {
+        parent[root] = find(parent[root]);
+        return parent[root];
+    }
+}
+
+void merge(int p, int q) {
+    int root_p, root_q;
+    root_p = find(p);
+    root_q = find(q);
+    if (root_p != root_q) {
+        parent[root_q] = root_p;
+    }
+    return;
 }
 
 int main() {
-    int i;
-    i = getint();
-    if (!func(99) && func(100)) i = 1; else i = 0;
+    int n = getint(), m = getint(), i;
+    int p, q;
+    i = 0;
+    while (i < n) {
+        parent[i] = i;
+        i = i + 1;
+    }
+    i = 0;
+    while (i < m) {
+        p = getint();
+        q = getint();
+        merge(p, q);
+        i = i + 1;
+    }
+
+    int clusters = 0;
+    i = 0;
+    while (i < n) {
+        if (parent[i] == i) clusters = clusters + 1;
+        i = i + 1;
+    }
+    putint(clusters);
     return 0;
 }
