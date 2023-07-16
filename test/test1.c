fe3904a33f70c65e40762d213639fba3759a8d7f
Valerian <1072977032@qq.com>
2023-07-16 20:45:28 +0800

    2023/7/15 finish memory debug

--- test/test1.c
+++ test/test1.c
@@ -1,34 +1,11 @@
-// reference: https://zhuanlan.zhihu.com/p/30748318
+const int MAX_WIDTH = 1024, MAX_HEIGHT = 1024;
+int image[MAX_WIDTH * MAX_HEIGHT], width, height;
 
-const int W = 192, H = 192, N = 24;
-
-const float PI = 3.14159265359, TWO_PI = 6.28318530718;
-const int MAX_STEP = 10;
-const float MAX_DISTANCE = 2.0;
-const float EPSILON = 1e-6;
-
-const int RAND_MAX = 100000007 - 1;
-int seed = 0;
-
-int rand() {
-    seed = (seed * 19980130 + 23333) % 100000007;
-    if (seed < 0) seed = seed + 100000007;
-    return seed;
-}
+const float PI = 3.14159265359, TWO_PI = 6.28318530718, EPSILON = 1e-6;
 
 float my_fabs(float x) {
     if (x > 0) return x;
     return -x;
-}
-
-float my_sqrt(float x) {
-    float t = x / 8 + 0.5 + 2 * x / (4 + x);
-    int c = 10;
-    while (c) {
-        t = (t + x / t) / 2;
-        c = c - 1;
-    }
-    return t;
 }
 
 float p(float x) { return 3 * x - 4 * x * x * x; }
@@ -50,68 +27,54 @@ float my_sin(float x) {
 
 float my_cos(float x) { return my_sin(x + PI / 2); }
 
-float circle_sdf(float x, float y, float cx, float cy, float r) {
-    float ux = x - cx, uy = y - cy;
-    return my_sqrt(ux * ux + uy * uy) - r;
-}
+//int read_image() {
+//    if (getch() != 80 || getch() != 50) return -1;
+//    width = getint();
+//    height = getint();
+//    if (width > MAX_WIDTH || height > MAX_HEIGHT || getint() != 255) return -1;
+//    int y = 0;
+//    while (y < height) {
+//        int x = 0;
+//        while (x < width) {
+//            image[y * width + x] = getint();
+//            x = x + 1;
+//        }
+//        y = y + 1;
+//    }
+//    return 0;
+//}
 
-void scene(float x, float y, float ret[]) {
-    float sd0 = circle_sdf(x, y, 0.4, 0.4, 0.10),
-            sd1 = circle_sdf(x, y, 0.6, 0.6, 0.05);
-    if (sd0 < sd1) {
-        ret[0] = sd0;
-        ret[1] = 3.0;
-    } else {
-        ret[0] = sd1;
-        ret[1] = 0.0;
-    }
+int rotate(int x, int y, float rad) {
+    float sinma = my_sin(rad), cosma = my_cos(rad);
+    int hwidth = width / 2, hheight = height / 2;
+    int xt = x - hwidth, yt = y - hheight;
+    int src_x = xt * cosma - yt * sinma + hwidth,
+            src_y = xt * sinma + yt * cosma + hheight;
+    if (src_x < 0 || src_x >= width || src_y < 0 || src_y >= height) return 0;
+    return image[src_y * width + src_x];
 }
-
-float trace(float ox, float oy, float dx, float dy) {
-    float t = 0.0;
-    int i = 0;
-    while (i < MAX_STEP && t < MAX_DISTANCE) {
-        float ret[2];
-        scene(ox + dx * t, oy + dy * t, ret);
-        if (ret[0] < EPSILON) return ret[1];
-        t = t + ret[0];
-        i = i + 1;
-    }
-    return 0.0;
-}
-
-float sample(float x, float y) {
-    float sum = 0.0;
-    int i = 0;
-    while (i < N) {
-        float rnd = rand();
-        float a = TWO_PI * (i + rnd / RAND_MAX) / N;
-        sum = sum + trace(x, y, my_cos(a), my_sin(a));
-        i = i + 1;
-    }
-    return sum / N;
-}
-
-void write_pgm() {
-    putch(80); putch(50); putch(10); // P2
-    putint(W); putch(32); putint(H); putch(32); putint(255); putch(10); // W H 255
-    int y = 0;
-    while (y < H) {
-        int x = 0;
-        while (x < W) {
-            float xx = x, yy = y;
-            int p = sample(xx / W, yy / H) * 255.0;
-            if (p > 255) p = 255;
-            putint(p);
-            putch(32);
-            x = x + 1;
-        }
-        putch(10);
-        y = y + 1;
-    }
-}
-
-int main() {
-    write_pgm();
-    return 0;
-}
+//
+//void write_pgm(float rad) {
+//    putch(80); putch(50); putch(10); // P2
+//    putint(width); putch(32); putint(height); putch(32); // width height
+//    putint(255); putch(10); // 255
+//    int y = 0;
+//    while (y < height) {
+//        int x = 0;
+//        while (x < width) {
+//            putint(rotate(x, y, rad));
+//            putch(32);
+//            x = x + 1;
+//        }
+//        putch(10);
+//        y = y + 1;
+//    }
+//}
+//
+//int main() {
+//    float rad = getfloat();
+//    getch();
+//    if (read_image() < 0) return -1;
+//    write_pgm(rad);
+//    return 0;
+//}
