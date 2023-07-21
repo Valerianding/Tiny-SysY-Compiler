01eef829784571a9c2315293fae94189e05072a0
Valerian <1072977032@qq.com>
2023-07-21 15:10:47 +0800

    2023/7/15 connect inliner with other optimization

--- tacmain.c
+++ tacmain.c
@@ -16,7 +16,7 @@
 #include "sideeffect.h"
 #include "fix_array.h"
 
-//#define ALL 0
+#define ALL 0
 extern FILE *yyin;
 extern HashMap *callGraph;
 extern HashSet *visitedCall;
@@ -135,88 +135,73 @@ int main(int argc, char* argv[]){
     }
 
     //建立phi之前
-    printf_llvm_ir(instruction_list,argv[4],1);
+//    printf_llvm_ir(instruction_list,argv[4],1);
 
     for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
         calculateNonLocals(currentFunction);
 
         mem2reg(currentFunction);
 
+        //Loop invariant code motion 需要使用live-out信息
         calculateLiveness(currentFunction);
 
         printLiveness(currentFunction);
-
-        //这里build CallGraphNode
-        buildCallGraphNode(currentFunction);
     }
 
-    //mem2reg之后，优化前
-    printf_llvm_ir(instruction_list,argv[4],1);
+//    //mem2reg之后，优化前
+//    printf_llvm_ir(instruction_list,argv[4],1);
 
 
     CheckGlobalVariable(instruction_list);
     JudgeXor(instruction_list);
     combineZext(instruction_list);
 
-    //need to put it into CheckGlobalVariable
-//    for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
-//        renameVariables(currentFunction);
-//    }
-//
-//    for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
-//        AlgorithmEliminate(currentFunction);
-//    }
 
-    for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
-        loop(currentFunction);
-        LICM(currentFunction);
-//        LoopConversion(currentFunction);
+    func_inline(instruction_list,255);
+
+
+    //重新构建Functi
+    Function *start = ReconstructFunction(instruction_list);
+
+    for(Function *currentFunction = start; currentFunction != NULL; currentFunction = currentFunction->Next){
+        //这里build CallGraphNode 需要在内联之后进行callgraph的
+        buildCallGraphNode(currentFunction);
     }
+
+    //OK 现在开始我们不会对
+//    printf_llvm_ir(instruction_list,argv[4],1);
+
 
     //先跑一次
     //cse cf
-//    for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next) {
-//        RunBasicPasses(currentFunction);
-//    }
+    for(Function *currentFunction = start; currentFunction != NULL; currentFunction = currentFunction->Next) {
+        RunBasicPasses(currentFunction);
+    }
 //
-//
-//    if(Optimize) {
-//        //基本块内inscomb ok，基本块间ing
-//        for (Function *currentFunction = block->Parent;
-//             currentFunction != NULL; currentFunction = currentFunction->Next) {
-//             RunOptimizePasses(currentFunction);
-//        }
-//
-//    }
 
-//IPO 暂时不开启
-//    travel();
+    if(Optimize) {
 
 
-//    for (Function *currentFunction = block->Parent;
-//         currentFunction != NULL; currentFunction = currentFunction->Next) {
-//        RunBasicPasses(currentFunction);
+        //IPO
+        travel();
+        for (Function *currentFunction = start;
+             currentFunction != NULL; currentFunction = currentFunction->Next) {
+             RunOptimizePasses(currentFunction);
+        }
 
-//
-//    if(Optimize){
-//        for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
-//            Clean(currentFunction);
-//        }
-//    }
+    }
 
 
 
-    //phi上的优化
-     printf_llvm_ir(instruction_list,argv[4],1);
-//
-//    if(Optimize){
-//        for(Function *currentFunction = block->Parent; currentFunction != NULL; currentFunction = currentFunction->Next){
-//            Clean(currentFunction);
-//        }
-//    }
+    for (Function *currentFunction = start;
+         currentFunction != NULL; currentFunction = currentFunction->Next) {
+        RunBasicPasses(currentFunction);
 
-//    printf_llvm_ir(instruction_list,argv[4],1);
-#ifdef ALL
+    }
+
+//phi上的优
+    printf_llvm_ir(instruction_list,argv[4],1);
+#if ALL
     //phi上的优化
 
     block = temp->inst->Parent;
@@ -239,17 +224,17 @@ int main(int argc, char* argv[]){
     }
 
     // Liveness 计算之后请注释掉我跑llvm
-    printf_llvm_ir(instruction_list,argv[4],1);
+//    printf_llvm_ir(instruction_list,argv[4],1);
 
 
     //lsy_begin
 //    printf("=================fix===================\n");
-//    fix_array(instruction_list);
+    fix_array(instruction_list);
 //    printf_llvm_ir(instruction_list,argv[4],0);
     //lsy_end
 
     //ljw_begin
-//    reg_control(instruction_list,temp);
+    reg_control(instruction_list,temp);
     //修改all_in_memory开启/关闭寄存器分配
     //ljw_end`1`
 
@@ -257,9 +242,9 @@ int main(int argc, char* argv[]){
 //    如果需要打印到文件里面，打开arm_open_file和arm_close_file,
 //    argv[3]里面直接给的就是汇编文件，直接打开就行，修改一下
 
-//    arm_open_file(argv[3]);
-//    arm_translate_ins(instruction_list,argv[3]);
-//    arm_close_file(argv[3]);
+    arm_open_file(argv[3]);
+    arm_translate_ins(instruction_list,argv[3]);
+    arm_close_file(argv[3]);
     //    ljf_end
 
 #endif
