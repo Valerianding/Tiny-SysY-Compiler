92d6dfb72491dbdc6808d40497fda263547f2a24
Valerian <1072977032@qq.com>
2023-08-03 17:37:06 +0800

    2023/8/3 ready to inline

--- ir/opt/inscomb.h
+++ ir/opt/inscomb.h
@@ -7,7 +7,7 @@
 //#include "function.h"
 //#include "utility.h"
 //#include "stdbool.h"
-//bool InstCombine(Function *currentFunction);
+
 //#endif //C22V1_INSCOMB_H
 
 
@@ -16,12 +16,17 @@
 //
 #ifndef C22V1_INSCOMB_H
 #define C22V1_INSCOMB_H
-#include <function.h>
-#include <stdbool.h>
+#include "function.h"
+#include "stdbool.h"
+#include "utility.h"
 void instruction_combination(Function *currentFunction);
 bool check(Instruction *instruction,int location);
 int get_const_location(Instruction* instruction);
 Value *get_value(Instruction *instruction);
 Value *get_const(Instruction *instruction);
 void combination(Instruction *instruction_A,Instruction * instruction_B);
+
+
+//djf
+bool InstCombine(Function *currentFunction);
 #endif //C22V1_INSCOMB_H
